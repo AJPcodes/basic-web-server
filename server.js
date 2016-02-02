8c64274c29295ab486962f50dd89be35c7092003
Allen Phelps <allenatmc@gmail.com>
2016-02-02 10:44:07 -0600

    playing with sending data as packets

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 //raw node server!
 "use strict";
 const http = require('http');
-//const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   console.log(req.url, req.method);
-  
-  debugger;
+  if (req.url === "/hello") {
+    res.writeHead(200, {
+     'Content-type': 'text/html' 
+    });
+    const counter = []; 
+    const msg = "<h1>Don't you miss dial-up?!</h1>";
+    msg.split('').forEach((x, i) => {
+      
+      setTimeout(() => {  
+      res.write(x);
+      counter.pop();
+      }, 300 * i);
+      counter.push(i);
+    });
 
-  res.writeHead(200, {
-   'Content-type': 'text/html' 
-  });
-  res.end('Herro! Worrd!');
+    var myVar = setInterval(function(){ 
+      if (counter.length === 0) {
+      res.end('<p>Delay!<p>');
+      }
+    }, 250);
+    
+  } else if (req.url === "/random") {
+    res.end(Math.random().toString());
+  } else {
+    res.writeHead(403);
+    res.end('Access Denied!');
+  }
   
 
 }).listen(3000, () => {
-
-  console.log(`Listening on port 3000`);
+  console.log(`Listening on port ${PORT}`);
 });
