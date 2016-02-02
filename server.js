66578f0e141699de28b0e65beed26a99d913df36
AJP <allenatmc@gmail.com>
2016-02-02 13:24:49 -0600

    add calendar access via route parameters

--- server.js
+++ server.js
@@ -1,39 +1,89 @@
 //raw node server!
 "use strict";
-const http = require('http');
+const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  if (req.url === "/hello") {
-    res.writeHead(200, {
-     'Content-type': 'text/html' 
-    });
-    const counter = []; 
-    const msg = "<h1>Don't you miss dial-up?!</h1>";
+app.get('/cal/:year?/:month?/', (req, res) => {
+
+  let yearArg = req.params.year;
+  let monthArg = req.params.month;
+
+  let args = [yearArg, monthArg];
+
+  if (monthArg === undefined) {
+    args = [yearArg];
+  }
+
+  if (yearArg === undefined) {
+    let today = new Date();
+    args = [today.getFullYear(), today.getMonth() + 1];
+  }
+
+  const validate = require('node-cal/lib/validateArgs').validate;
+  const getMonth = require('node-cal/lib/month').getMonth;
+  const getYear = require('node-cal/lib/year').getYear;
+  const validatedArgs = validate(args);
+
+
+  if (validatedArgs){
+     yearArg = validatedArgs[0];
+      monthArg = validatedArgs[1];
+
+
+
+    if (yearArg && monthArg) {
+      let output = getMonth(yearArg, monthArg);
+      res.send("<pre>" + output + "</pre>");
+    }
+
+    else if (yearArg && !monthArg) {
+      let output = getYear(yearArg);
+      res.send("<pre>" + output + "</pre>");
+    }
+  }
+
+  else {
+    res.send("Invalid parameters, Try again");
+  }
+});
+
+app.get("/hello", (req, res) => {
+
+    console.log(req.query);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    const counter = [];
+    const msg = "<h1>Hello " + req.query.name + "</h1>";
     msg.split('').forEach((x, i) => {
-      
-      setTimeout(() => {  
+
+      setTimeout(() => {
       res.write(x);
       counter.pop();
       }, 300 * i);
       counter.push(i);
     });
 
-    var myVar = setInterval(function(){ 
+    var interval = setInterval(function(){
       if (counter.length === 0) {
-      res.end('<p>Delay!<p>');
+      res.end('<p>Done<p>');
       }
     }, 250);
-    
-  } else if (req.url === "/random") {
-    res.end(Math.random().toString());
-  } else {
-    res.writeHead(403);
-    res.end('Access Denied!');
-  }
-  
+});
 
-}).listen(3000, () => {
+app.get("/random/:x/:y", (req, res) => {
+    console.log(req.params.x);
+    console.log(req.params.y);
+    res.send(200, Math.floor(Math.random() * (req.params.y - req.params.x)) + parseInt(req.params.x) + 1);
+
+});
+
+app.get('/secret', (req, res) => {
+
+  res.writeHead(403);
+  res.end('Access Denied');
+});
+
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
