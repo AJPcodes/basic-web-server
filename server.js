116c49ba52ecd2e7544b95cdc5b77e04a83b1f58
AJP <allenatmc@gmail.com>
2016-02-03 10:59:42 -0600

    abstract cal-tab logic to separate module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //raw node server!
 "use strict";
 const app = require('express')();
+const getCal = require('./lib/cal.js');
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
@@ -15,46 +16,8 @@ app.get('/', (req, res) => {
 
 app.get('/cal/:year?/:month?/', (req, res) => {
 
-  let yearArg = req.params.year;
-  let monthArg = req.params.month;
+  res.send(getCal(req.params.year,req.params.month ));
 
-  let args = [yearArg, monthArg];
-
-  if (monthArg === undefined) {
-    args = [yearArg];
-  }
-
-  if (yearArg === undefined) {
-    let today = new Date();
-    args = [today.getFullYear(), today.getMonth() + 1];
-  }
-
-  const validate = require('node-cal/lib/validateArgs').validate;
-  const getMonth = require('node-cal/lib/month').getMonth;
-  const getYear = require('node-cal/lib/year').getYear;
-  const validatedArgs = validate(args);
-
-
-  if (validatedArgs){
-     yearArg = validatedArgs[0];
-      monthArg = validatedArgs[1];
-
-
- 
-    if (yearArg && monthArg) {
-      let output = getMonth(yearArg, monthArg);
-      res.send("<pre>" + output + "</pre>");
-    }
-
-    else if (yearArg && !monthArg) {
-      let output = getYear(yearArg);
-      res.send("<pre>" + output + "</pre>");
-    }
-  }
-
-  else {
-    res.send("Invalid parameters: <br> Try again useing cal/YYYY/MM");
-  }
 });
 
 app.get("/hello", (req, res) => {
