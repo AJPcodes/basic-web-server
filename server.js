0482d55b510ccea686be998be9933d706670ef83
AJP <allenatmc@gmail.com>
2016-02-04 12:32:14 -0600

    add example contact and photo upload forms

--- server.js
+++ server.js
@@ -5,17 +5,30 @@ const sassMiddleware = require('node-sass-middleware');
 const path = require('path');
 const getCal = require('./lib/cal.js');
 const PORT = process.env.PORT || 3000;
+//used to add a 'body' key to post req objects with form data
+const bodyParser = require('body-parser');
+//used to handle multi-posts (file uploads)
+const multer = require('multer');
+const upload = multer({dest: 'tmp/uploads/'});
 
 const app = express();
+
+const appTitle = 'C Allen Dar'
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//local allows you to set global variables usable by the renderer.
+//The object will be passed to every render
+app.locals.title = "C (Allen) Dar";
+
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //sass setup
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
+  indentedSyntax: true, //used to do either sass or scss
   sourceMap: true
 }));
 
@@ -23,17 +36,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'C (Allen) Dar',
     date: new Date()
 
   });
 });
 
+
+//regular form using body parser
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    date: new Date()
+  });
+});
+
+app.post('/contact', (req, res) => {
+  let userName = req.body.name;;
+  res.send(`<h1>Thanks ${userName}</h1>`)
+});
+
+//file upload form using multer
+app.get('/sendphoto', (req, res) => {
+  res.render('sendphoto', {
+    date: new Date()
+  });
+});
+
+app.post('/sendphoto', upload.single('image'), (req, res) => {
+    res.send('<h1>Thanks for the Photo</h1>')
+});
+
+//playing with query params
 app.get('/cal/:year?/:month?/', (req, res) => {
   const output = getCal(req.params.year,req.params.month);
 
   res.render('calDisplay', {
-    title: 'C (Allen) Dar',
     calString: output
   });
 });
